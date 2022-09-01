1eb5469ad9b75b62ed7dadddb5ea06110ac975b9
Prashanth G <prashanthg321@gmail.com>
2022-09-01 14:53:53 +0530

    Excercise 3 Completed

---
+++ C_sharp_Excercise/Excercise3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace C_sharp_Excercise
+{
+    internal class Excercise3
+    {
+        public static void Main()
+        {
+            int a = 60;
+            int b = 6;
+            Console.WriteLine("{0}/{1} = {2}",a,b,a/b);
+            Console.ReadKey();
+        }
+    }
+}
