466c3809f72973369e12253412a78872652848e4
Prashanth G <prashanthg321@gmail.com>
2022-09-01 15:19:52 +0530

    Excercise 7 completed

---
+++ C_sharp_Excercise/Excercise7.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace C_sharp_Excercise
+{
+    internal class Excercise7
+    {
+        public static void Main2()
+        {
+            Console.WriteLine("Input the first number:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the second number:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Arithmetic(a, b);
+            Console.ReadKey();
+        }
+        private static void Arithmetic(int i,int j)
+        {
+            Console.WriteLine("{0}+{1} = {2}", i, j, i + j);
+            Console.WriteLine("{0}-{1} = {2}", i, j, i - j);
+            Console.WriteLine("{0}*{1} = {2}", i, j, i * j);
+            Console.WriteLine("{0}/{1} = {2}", i, j, i / j);
+            Console.WriteLine("{0}mod{1} = {2}", i, j, i % j);
+        }
+    }
+}
