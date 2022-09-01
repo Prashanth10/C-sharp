339b71cdc3f9a0d2eb8dc0f9f2f62d305cb397db
Prashanth G <prashanthg321@gmail.com>
2022-09-01 15:24:29 +0530

    Excercise 8 completed

---
+++ C_sharp_Excercise/Excercise8.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace C_sharp_Excercise
+{
+    internal class Excercise8
+    {
+        public static void Main2()
+        {
+            Console.WriteLine("Enter the number:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("{0}*{1} = {2}",a,i,a*i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
