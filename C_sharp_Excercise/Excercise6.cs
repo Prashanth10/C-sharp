42933ac2ab33cc47ddd2cbe197d939ec24461d79
Prashanth G <prashanthg321@gmail.com>
2022-09-01 15:13:02 +0530

    Excercise 6 completed

---
+++ C_sharp_Excercise/Excercise6.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace C_sharp_Excercise
+{
+    internal class Excercise6
+    {
+        public static void Main2()
+        {
+            Console.WriteLine("Input the first number to multiply:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the second number to multiply:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the third number to multiply:");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Expected Output: {0}*{1}*{2} = {3}",a,b,c,a*b*c);
+            Console.ReadKey();
+        }
+    }
+}
