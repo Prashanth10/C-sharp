30102a1022246dbde24e9d306e82a7cf01996dde
Prashanth G <prashanthg321@gmail.com>
2022-09-01 14:48:21 +0530

    Excercise 2 completed

---
+++ C_sharp_Excercise/Excercise1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace C_sharp_Excercise
+{
+    internal class Excercise1
+    {
+        static void Main2(string[] args)
+        {
+            string name = "Prashanth";
+            Console.WriteLine("Hello {0}", name);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_sharp_Excercise/Excercise2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace C_sharp_Excercise
+{
+    internal class Excercise2
+    {
+        public static void Main()
+        {
+            int a = 10;
+            int b = 5;
+            Console.WriteLine("Sum is {0}",a+b);
+            Console.ReadKey();
+        }
+    }
+}
