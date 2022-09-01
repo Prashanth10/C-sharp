5ea41464ae4b409aaea67a3145506191939bbd68
Prashanth G <prashanthg321@gmail.com>
2022-09-01 15:05:31 +0530

    Excercise 5 completed

---
+++ C_sharp_Excercise/Excercise5.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace C_sharp_Excercise
+{
+    internal class Excercise5
+    {
+        public static void Main2()
+        {
+            int a = 5, b = 6;
+            int k = a;
+            a = b;
+            b = k;
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+            Console.ReadKey();
+        }
+    }
+}
