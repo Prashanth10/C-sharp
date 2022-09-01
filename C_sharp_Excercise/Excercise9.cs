9115b59c1167f3f8ddb7dde3f77081c7050b7c27
Prashanth G <prashanthg321@gmail.com>
2022-09-01 15:31:10 +0530

    Excercise 9 completed

---
+++ C_sharp_Excercise/Excercise9.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace C_sharp_Excercise
+{
+    internal class Excercise9
+    {
+        public static void Main2()
+        {
+            Console.WriteLine("Enter the First number:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second number:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Third number:");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Fourth number:");
+            int d = Convert.ToInt32(Console.ReadLine());
+            float f = (a + b + c + d) / 4f;
+            Console.WriteLine("The average of {0},{1},{2},{3} is: {4}",a,b,c,d,f);
+            Console.ReadKey();
+        }
+    }
+}
