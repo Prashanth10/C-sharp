a55612900fb0d330a2efada5b25db0130707993b
Prashanth G <prashanthg321@gmail.com>
2022-09-02 12:18:39 +0530

    Excercise 23 completed

---
+++ C_sharp_Excercise/Excercise23.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace C_sharp_Excercise
+{
+    internal class Excercise23
+    {
+        public static void Main2()
+        {
+            Console.WriteLine("Input upto the table number starting from 1 :");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Multiplication table from 1 to {0}",n);
+            for(int i = 1; i <= 10; i++)
+            {
+                for(int j = 1; j <= n; j++)
+                {
+                    Console.Write("{0}*{1} = {2}  ", j, i, j * i);
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadKey();
+        }
+    }
+}
