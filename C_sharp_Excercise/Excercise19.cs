ff54c115571f9c2ccda9e9e54a58382ca958635e
Prashanth G <prashanthg321@gmail.com>
2022-09-02 11:40:53 +0530

    Excercise 19 completed

---
+++ C_sharp_Excercise/Excercise19.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace C_sharp_Excercise
+{
+    internal class Excercise19
+    {
+        public static void Main2()
+        {
+            Console.WriteLine("Input the value for X coordinate :");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the value for Y coordinate :");
+            int y = Convert.ToInt32(Console.ReadLine());
+            if (x >= 0 && y >= 0)
+                Console.WriteLine("The coordinate point ({0},{1}) lies in the First quadrant.", x, y);
+            else if (x < 0 && y >= 0)
+                Console.WriteLine("The coordinate point ({0},{1}) lies in the Second quadrant.", x, y);
+            else if (x < 0 && y < 0)
+                Console.WriteLine("The coordinate point ({0},{1}) lies in the Third quadrant.", x, y);
+            else
+                Console.WriteLine("The coordinate point ({0},{1}) lies in the Fourth quadrant.", x, y);
+            Console.ReadKey();
+        }
+    }
+}
