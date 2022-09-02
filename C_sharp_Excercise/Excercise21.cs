f1cd84a89c04fe195c81d3d2debf92e6457ce3a4
Prashanth G <prashanthg321@gmail.com>
2022-09-02 12:09:44 +0530

    Excercise 21 completed

---
+++ C_sharp_Excercise/Excercise21.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace C_sharp_Excercise
+{
+    internal class Excercise21
+    {
+        public static void Main2()
+        {
+            Console.WriteLine("Input number of terms:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i <= n; i++)
+            {
+                Console.WriteLine("Number is : {0} and cube of the {1} is :{2}",i,i,i*i*i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
