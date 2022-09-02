8db16419b0aac2bbd93ee8327b9b06e37f3d3d3a
Prashanth G <prashanthg321@gmail.com>
2022-09-02 10:46:01 +0530

    Excercise 12 completed

---
+++ C_sharp_Excercise/Excercise12.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace C_sharp_Excercise
+{
+    internal class Excercise12
+    {
+        public static void Main2()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0)
+                Console.WriteLine("{0} is an even integer", a);
+            else
+                Console.WriteLine("{0} is an odd integer", a);
+            Console.ReadKey();
+        }
+    }
+}
