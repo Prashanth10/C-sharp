f16d3fe65683e678b5c43705dd1b8d414e511a50
Prashanth G <prashanthg321@gmail.com>
2022-09-02 11:04:52 +0530

    Excercise 16 completed

---
+++ C_sharp_Excercise/Excercise16.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace C_sharp_Excercise
+{
+    internal class Excercise16
+    {
+        public static void Main2()
+        {
+            int m = Convert.ToInt32(Console.ReadLine());
+            if (m < 0)
+                Console.WriteLine("The value of n = -1");
+            else if(m==0)
+                Console.WriteLine("The value of n = 0");
+            else
+                Console.WriteLine("The value of n = 1");
+            Console.ReadKey();
+        }
+    }
+}
