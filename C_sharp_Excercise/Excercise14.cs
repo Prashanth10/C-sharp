32c9f165f8b5ae0beb331e9c1f2bcd5cbfc0ed7f
Prashanth G <prashanthg321@gmail.com>
2022-09-02 10:59:03 +0530

    Excercise 14 completed

---
+++ C_sharp_Excercise/Excercise14.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace C_sharp_Excercise
+{
+    internal class Excercise14
+    {
+        public static void Main2()
+        {
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0)
+                Console.WriteLine("{0} is a leap year", year);
+            else
+                Console.WriteLine("{0} is not a leap year", year);
+            Console.ReadKey();
+        }
+    }
+}
