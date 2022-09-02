ebac6ef4bce3ec5b67fcc9f514cbc58d7d37c6e3
Prashanth G <prashanthg321@gmail.com>
2022-09-02 10:49:57 +0530

    Excercise 13 completed

---
+++ C_sharp_Excercise/Excercise13.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace C_sharp_Excercise
+{
+    internal class Excercise13
+    {
+        public static void Main2()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a < 0)
+                Console.WriteLine("{0} is a negative integer",a);
+            else
+                Console.WriteLine("{0} is a positive integer", a);
+            Console.ReadKey();
+        }
+    }
+}
