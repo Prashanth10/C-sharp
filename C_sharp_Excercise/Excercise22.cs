608ba534a5a824017fbccf740e1b23d7050d7046
Prashanth G <prashanthg321@gmail.com>
2022-09-02 12:12:00 +0530

    Excercise 22 completed

---
+++ C_sharp_Excercise/Excercise22.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace C_sharp_Excercise
+{
+    internal class Excercise22
+    {
+        public static void Main2()
+        {
+            Console.WriteLine("Input the number (Table to be calculated) :");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("{0}*{1} = {2}", n, i, i * i * i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
