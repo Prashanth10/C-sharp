349542ebb89200ffcf55b430b7287ca9e08fdb02
Prashanth G <prashanthg321@gmail.com>
2022-09-02 13:14:39 +0530

    Excercise 30 completed

---
+++ C_sharp_Excercise/Excercise30.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace C_sharp_Excercise
+{
+    internal class Excercise30
+    {
+        public static void Main()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                for (int k = 0; k < n - i; k++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
