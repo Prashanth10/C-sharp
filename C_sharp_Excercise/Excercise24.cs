c2949e338675eb2a77a07eb5891c61d0b1410093
Prashanth G <prashanthg321@gmail.com>
2022-09-02 12:24:55 +0530

    Excercise 24 completed

---
+++ C_sharp_Excercise/Excercise24.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace C_sharp_Excercise
+{
+    internal class Excercise24
+    {
+        public static void Main2()
+        {
+            Console.Write("Input number of terms:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            Console.Write("The odd numbers are :");
+            for (int i = 1; i <= 2*n; i+=2)
+            {
+                Console.Write("{0} ",i);
+                sum += i;
+            }
+            Console.WriteLine();
+            Console.WriteLine("The Sum of odd Natural Number upto 10 terms : {0}",sum);
+            Console.ReadKey();
+        }
+    }
+}
