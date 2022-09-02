f7216e157014cb964c6a38343c77fbb6fbba5bf7
Prashanth G <prashanthg321@gmail.com>
2022-09-02 11:01:28 +0530

    Excercise 15 completed

---
+++ C_sharp_Excercise/Excercise15.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace C_sharp_Excercise
+{
+    internal class Excercise15
+    {
+        public static void Main2()
+        {
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age < 18)
+                Console.WriteLine("You are not eligible for casting your vote");
+            else
+                Console.WriteLine("Congratulation! You are eligible for casting your vote.");
+            Console.ReadKey();
+        }
+    }
+}
