b7c77b53f79826b2263c534eba8605fdabfdbdab
Prashanth G <prashanthg321@gmail.com>
2022-09-02 11:28:40 +0530

    Excercise 17 completed

---
+++ C_sharp_Excercise/Excercise17.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace C_sharp_Excercise
+{
+    internal class Excercise17
+    {
+        public static void Main2()
+        {
+            int height = Convert.ToInt32(Console.ReadLine());
+            if (height < 150)
+                Console.WriteLine("The person is Dwarf.");
+            else if(height<180)
+                Console.WriteLine("The person is Normal.");
+            else
+                Console.WriteLine("The person is Tall.");
+            Console.ReadKey();
+        }
+    }
+}
